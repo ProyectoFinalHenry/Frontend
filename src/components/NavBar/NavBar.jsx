ff02b62e47c17334503fda26ffe614d52a8859e0
JhonatanDzG <gizudajhovi@gmail.com>
2023-08-17 20:58:22 -0500

    feat: detail-view

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link, useLocation } from 'react-router-dom'; 
 import './NavBar.css'
+import { PathsToNoFill } from '../../constants/NavbarPathToFill.constants';
 
 const NavBar = () => {
+
+const location = useLocation();
+
     return (
-        <div className="navbar-container">
+        <div className={"navbar-container " + (PathsToNoFill.includes(location.pathname) ? "" : "fill")}>
           <div className='navbar-logo'>
             <img src="/assets/images/logo-3-white.png" alt="logo" />
           </div>
